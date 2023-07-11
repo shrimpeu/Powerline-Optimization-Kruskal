95a838081a7a97c80387e35d1e5751c1434e91ee
Apologranzze <104176563+Apologranzze@users.noreply.github.com>
2023-07-11 11:10:02 +0800

    Cleaning

--- Kruskal in pygame.py
+++ Kruskal in pygame.py
@@ -18,7 +18,7 @@ class MyEntry(object):
         self.fontstyle = fontstyle
         self.fontsize = fontsize
         self.bool = bool
-        self.color_Text = color_text
+        self.color_text = color_text
         self.color_bg = color_bg
         self.x = x
         self.y = y
@@ -33,18 +33,18 @@ class MyEntry(object):
 
     def events(self, event):
         if self.x <= mousepos[0] <= self.x + self.width and self.y <= mousepos[1] <= self.y + self.height:
-            # Change the button's color when clicking the button
+            # Change the button's color when hovering over the button
             pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_IBEAM)
             if event.type == pygame.MOUSEBUTTONDOWN and self.active_state is False:
-                self.active_state = True
+                self.active_state = True  # active object
                 self.stringsize = 0
                 self.string = ""
                 print(self.active_state)
         else:
+            # Reset the button's color when the mouse is not hovering over it
             if event.type == pygame.MOUSEBUTTONDOWN and self.active_state is True:
-                self.active_state = False
+                self.active_state = False  # inactive object
                 print(self.active_state)
-            # Reset the button's color when the mouse is not hovering over it and becomes inactive
             pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
 
         # pressing keyboard down
@@ -63,15 +63,15 @@ class MyEntry(object):
 
     def draw(self, screen):
         font = pygame.font.SysFont(self.fontstyle, self.fontsize)
-        text = font.render(str(self.string), self.bool, self.color_Text, self.color_bg)
+        text = font.render(str(self.string), self.bool, self.color_text, self.color_bg)
         textrect = text.get_rect()
         textrect.center = (self.x + self.width // 2, self.y + self.height // 2)
         if self.active_state is True:
             entry_color = self.entry_color_active
-            self.color_Text = (0, 0, 0)
+            self.color_text = (0, 0, 0)
         else:
             entry_color = self.entry_color_inactive
-            self.color_Text = (255, 255, 255)
+            self.color_text = (255, 255, 255)
         # background
         pygame.draw.rect(screen, entry_color, (self.x, self.y, self.width, self.height), 0, self.borderradius)
         # outline
@@ -82,8 +82,6 @@ class MyEntry(object):
 
 mousemovementpos = ()
 mouseclickpos = ()
-active_state = False
-stringsize = 0
 
 # classes needed to be called once, and it's properties to be drawn in a loop
 sourceentry = MyEntry('source', 'monospace', 30, True, (255, 255, 255), None, 930, 20, 125, 35, (175, 175, 175),
